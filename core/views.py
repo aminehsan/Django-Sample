5d7470b89944467c4f5cdd98545478bc85462f89
Amin Ehsan <Mr.AminEhsan@gmail.com>
2026-08-01 15:42:36 +0330

    Add views.py

---
+++ core/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(
+        request,
+        "index.html",
+        {
+            "title": "index page",
+            "username": "admin",
+        },
+    )
