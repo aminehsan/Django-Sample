042db0db5f34e8c7d77642d15dff9bf101af521a
Amin Ehsan <Mr.AminEhsan@gmail.com>
2026-08-03 15:26:31 +0330

    Create Ad model

--- ads/models.py
+++ ads/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Ad(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
