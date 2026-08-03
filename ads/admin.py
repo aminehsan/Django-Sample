b96a19b006b9b914325da58bdc71398d26eec8e3
Amin Ehsan <Mr.AminEhsan@gmail.com>
2026-08-03 15:45:58 +0330

    Register Ad model

--- ads/admin.py
+++ ads/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Ad
 
-# Register your models here.
+
+@admin.register(Ad)
+class AdAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "created_at",
+        "updated_at",
+        "title",
+        "description",
+    )
+    list_filter = (
+        "created_at",
+        "updated_at",
+    )
+    search_fields = ("title",)
