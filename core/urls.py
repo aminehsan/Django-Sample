85fab4893f8cd764c47d863131ad46bdc83c2141
Amin Ehsan <Mr.AminEhsan@gmail.com>
2026-08-08 11:28:02 +0330

    Remove index page

--- core/urls.py
+++ core/urls.py
@@ -17,10 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from .views import index
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("index/", index),
     path("ads/", include("ads.urls")),
 ]
